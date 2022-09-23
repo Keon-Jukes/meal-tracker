f2836fc7a3d7a49dcb3ce26d838cfc33c5096de9
keonjukes <keonofthephanerozoiceon@gmail.com>
2022-09-22 20:51:24 -0700

    useState down pack

--- src/useStateComponent.tsx
+++ src/useStateComponent.tsx
@@ -4,12 +4,19 @@ function UseStateComponent(){
     //add type number to specify what will go into the array
     const [arr, arrSet] = useState<number[]>([]);
 
+    const [name, nameSet] = useState<string | null>(null);
+
     return(
         <div>
             <div>
                 <button onClick={() => arrSet([...arr, arr.length + 1])}>
                     Add to array</button>
                 {JSON.stringify(arr)}
+            </div>
+            <div>
+                <button onClick={() => nameSet("keon")}>
+                    Set Name</button>
+                {JSON.stringify(name)}
             </div>
         </div>
     )
